714ef5b04d86397536ed46432579ca980b1017a2
M4her <maher44hmed@gmail.com>
2025-06-21 01:28:14 +0600

    colors part done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ $('.counter').counterUp({
     delay: 10,
     time: 1000
 });
+
+// slider part 1
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  asNavFor: '.slider-for',
+  centerMode: true,
+  focusOnSelect: true
+});
